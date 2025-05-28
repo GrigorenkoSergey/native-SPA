400bf456a2fb92d64affbc579b05caff45bc6b66
Sergey Grigorenko <crazybuilder51@gmail.com>
2025-05-28 08:24:10 +0300

    updated expand logic

--- src/components/custom-autocomplete/index.js
+++ src/components/custom-autocomplete/index.js
@@ -1,9 +1,25 @@
 import "./style.css";
 import template from "./template.html?raw";
 
+import { listenClickOutsideOnce } from "../../utils/listenClickOutsideOnce";
+
 class CustomAutocomplete extends HTMLElement {
   connectedCallback() {
     this.innerHTML = template;
+    this.input = this.querySelector("input");
+
+    this.addEventListener("click", this.handleClick);
+  }
+
+  handleClick(event) {
+    const { target } = event;
+    this.classList.add("expanded");
+
+    listenClickOutsideOnce(this, (element) => element.classList.remove("expanded"));
+
+    if (target.tagName === "LI") {
+      this.input.value = target.textContent;
+    }
   }
 }
 
