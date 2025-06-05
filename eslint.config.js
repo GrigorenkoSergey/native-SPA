22a53f3c797f5451745dab8da03f8e2346819823
Sergey Grigorenko <crazybuilder51@gmail.com>
2025-06-05 08:47:10 +0300

    added more eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,28 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import prettier from 'eslint-config-prettier';
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: {
+      js,
+      prettier,
+    },
+    extends: [
+      "js/recommended",
+      "plugin:prettier/recommended",
+    ],
+    rules: {
+      "prettier/prettier": "error",
+    }
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
 ]);
