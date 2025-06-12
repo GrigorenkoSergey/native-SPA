b51524bfc340f260f42535f12d1ff6d94299ead8
Sergey Grigorenko <crazybuilder51@gmail.com>
2025-06-12 14:31:12 +0300

    fixed errors in eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,22 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-import prettier from 'eslint-config-prettier';
-
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: {
-      js,
-      prettier,
-    },
-    extends: [
-      "js/recommended",
-      "plugin:prettier/recommended",
-    ],
-    rules: {
-      "prettier/prettier": "error",
-    }
-  },
-  {
-    files: ["**/*.{js,mjs,cjs}"],
     languageOptions: {
-      globals: globals.browser
-    }
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      "prettier/prettier": "error",
+    },
   },
 ]);
