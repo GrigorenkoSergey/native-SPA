f19461276d544772e00ea0edc40b18a48b46fda3
Sergey Grigorenko <crazybuilder51@gmail.com>
2025-06-20 06:30:02 +0300

    added outer links

--- src/utils/applyRouting.js
+++ src/utils/applyRouting.js
@@ -30,6 +30,9 @@ document.addEventListener("click", async event => {
   const link = event.target.closest("a");
   if (!link) return;
 
+  const isExternalLink = new URL(link.href).origin !== window.location.origin;
+  if (isExternalLink) return;
+
   event.preventDefault();
 
   const newUrl = new URL(link.href, window.location.href);
