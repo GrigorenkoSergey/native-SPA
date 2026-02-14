2b1b054bdc7c584fd33c0f2f77d3e4d3e3e33f32
Sergey Grigorenko <crazybuilder51@gmail.com>
2026-02-14 13:18:32 +0300

    Добавил простейшее отображение дат

--- src/components/custom-calendar/index.ts
+++ src/components/custom-calendar/index.ts
@@ -3,20 +3,88 @@ import styles from "./style.css?raw";
 import { attachStyles, initCustomElement } from "@/utils/customElementHelpers";
 import { assert } from "@/utils/assert";
 
+const msInDay = 24 * 60 * 60 * 1000;
+
+const monthDates = (d: string) => {
+  const date = new Date(d);
+
+  const startOfMonth = new Date(date).setDate(1);
+  const endOfMonth = new Date(date).setFullYear(date.getFullYear(), date.getMonth() + 1, 0);
+  const daysInCurrentMonth = (endOfMonth - startOfMonth) / (msInDay) + 1;
+
+  const weekDayOfMonthStart = new Date(startOfMonth).getDay();
+  const weekDayOfMonthEnd = new Date(endOfMonth).getDay();
+  const daysFromPrevMonth = weekDayOfMonthStart === 0 ? 6 : weekDayOfMonthStart - 1;
+  const daysFromNextMonth = weekDayOfMonthEnd === 0  ? 0 : 7 - weekDayOfMonthEnd;
+
+  const daysInCalendar = daysFromPrevMonth + daysInCurrentMonth + daysFromNextMonth; 
+  const dates = Array.from(
+    {length: daysInCalendar}, 
+    (_, i) => new Date(startOfMonth - (daysFromPrevMonth - i) * msInDay),
+  );
+
+  return dates;
+};
+
 // паттерн grid https://www.w3.org/WAI/ARIA/apg/patterns/grid/
 export class CustomCalendar extends HTMLElement {
   [key: string]: unknown;
   ["constructor"]!: typeof CustomCalendar;
+
+  shadowRoot!: ShadowRoot;
+  isInnerAttrSet = false;
+  isRendered = false;
+
+  state = {
+    selectedDate: null,
+  };
 
   constructor() {
     super();
     this.attachShadow({mode: "open"});
   }
 
+  static get observedAttributes() {
+    return ["date"];
+  }
+
   connectedCallback() {
-    assert(this.shadowRoot);
     this.shadowRoot.innerHTML = template;
     attachStyles(this, styles);
+    this.render();
+    this.isRendered = true;
+  }
+
+  render() {
+    this.renderNewMonth();
+  }
+
+  renderNewMonth() {
+    const visibledDates = monthDates(new Date().toLocaleString());
+    const weeksInMonth = visibledDates.length / 7;
+
+    let ptr = 0;
+    const tbody = document.createElement("tbody");;
+
+    for (let week = 0; week < weeksInMonth; week++) {
+      const tr = document.createElement("tr");
+
+      for (let day = 0; day < 7; day++) {
+        const pointedDate = visibledDates[ptr];
+
+        const td = document.createElement("td");
+        td.dataset.date = pointedDate.toDateString();
+        td.textContent = String(pointedDate.getDate());
+
+        tr.append(td);
+        ptr += 1;
+      }
+
+      tbody.append(tr);
+    }
+    const oldTbody = this.shadowRoot.querySelector("tbody");
+    assert(oldTbody);
+    oldTbody.replaceWith(tbody);
   }
 }
 
